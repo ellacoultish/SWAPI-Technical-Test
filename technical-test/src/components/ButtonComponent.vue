a848ecbda080f5ece1935492645004ddf3a5f829
Ella Coultish <ella.coultish@gmail.com>
2023-01-15 12:11:49 +0000

    restyle

---
+++ technical-test/src/components/ButtonComponent.vue
@@ -0,0 +1,18 @@
+<template>
+     <v-btn elevation="2" @click="$emit('button-clicked')"> 
+      <v-icon right dark>
+        {{icon}}
+      </v-icon>
+      {{text}}
+    </v-btn>
+</template>
+
+<script>
+  export default {
+    name: 'ButtonComponent',
+    props: ['text','icon'],
+    emits: ['button-clicked'],
+  }
+</script>
+<style lang="scss">
+</style>
--- technical-test/src/components/CharacterComponent.vue
+++ technical-test/src/components/CharacterComponent.vue
@@ -1,47 +1,68 @@
 <template>
   <div class="character-parent" v-if="!this.$store.state.loading">
-    <v-btn elevation="2" @click="navigateHome()"> 
-      <v-icon right dark>
-        mdi-arrow-left
-      </v-icon>
-      Go Back
-    </v-btn>
-    <h2>{{ userData.name }}</h2>
+    <ButtonComponent class="back-button" text="Go Back" icon="mdi-arrow-left" @button-clicked="navigateHome()"></ButtonComponent>
     <div class="content-wrapper">
-    <v-card max-width="375" class="mx-auto" >
-    <v-list two-line>
+  <v-card max-width="500px" class="d-flex justify-center">
+    <v-list>
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title>{{userData.birth_year}}</v-list-item-title>
+          <v-list-item-title>{{ userData?.name}}</v-list-item-title>
+          <v-list-item-subtitle>Name</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>{{userData?.birth_year}}</v-list-item-title>
           <v-list-item-subtitle>Date of Birth</v-list-item-subtitle>
         </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
         <v-list-item-content>
-          <v-list-item-title>{{userData.height}}</v-list-item-title>
+          <v-list-item-title>{{userData?.height}}</v-list-item-title>
           <v-list-item-subtitle>Height</v-list-item-subtitle>
         </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
         <v-list-item-content>
-          <v-list-item-title>{{userData.mass}}</v-list-item-title>
+          <v-list-item-title>{{userData?.mass}}</v-list-item-title>
           <v-list-item-subtitle>Mass</v-list-item-subtitle>
         </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
         <v-list-item-content>
-          <v-list-item-title>{{userData.homeworld}}</v-list-item-title>
-          <v-list-item-subtitle>Home World</v-list-item-subtitle>
+          <v-list-item-title>{{userData?.gender}}</v-list-item-title>
+          <v-list-item-subtitle>Gender</v-list-item-subtitle>
         </v-list-item-content>
         </v-list-item>
+        <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>{{userData?.skin_color}}</v-list-item-title>
+          <v-list-item-subtitle>Skin Colour</v-list-item-subtitle>
+        </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>{{userData?.hair_color}}</v-list-item-title>
+          <v-list-item-subtitle>Hair Colour</v-list-item-subtitle>
+        </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>{{$store.getters.getCharacterLikesById($route.params.id) ?? 0}}</v-list-item-title>
+          <v-list-item-subtitle>Likes</v-list-item-subtitle>
+        </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+        <v-list-item-content>
+          <ButtonComponent text="Write Review" icon="mdi-pencil-plus"></ButtonComponent>
+          <ButtonComponent text="Like Character" icon=" mdi-thumb-up"  @button-clicked="$store.commit('likeCharacter',$route.params.id)"></ButtonComponent>
+          </v-list-item-content>
+          </v-list-item>
         </v-list>
+
     </v-card>
-    <v-btn> 
-      <v-icon right dark>
-        mdi-pencil-plus
-      </v-icon>
-      Write Review
-    </v-btn>
-      <v-btn @click="$store.commit('likeCharacter',$route.params.id)  "> 
-      <v-icon right dark>
-        mdi-thumb-up
-      </v-icon>
-      Like Character
-    </v-btn>
+ 
+
     </div>
   </div>
   <div class="center" v-else>
@@ -53,14 +74,16 @@
 </template>
 
 <script>
+import ButtonComponent from '@/components/ButtonComponent.vue'
   export default {
     name: 'CharacterComponent',
-
+    components: {
+      ButtonComponent
+    },
     data() {
       return {
         userData: null,
-        loading: true,
-        breadcrumbItems: []
+        loading: true
       }
     },
     methods: {
@@ -71,12 +94,6 @@
         this.userData = finalRes;
         this.$store.commit('loading',{state:false});
         console.log(this.userData);
-        this.breadcrumbItems.push({
-        disabled: false,
-        href: "",
-        link: true,
-        text: this.userData.name
-      })
       },
       navigateHome(){
         this.$router.push('/');
@@ -84,18 +101,12 @@
     },
     mounted() {
       this.getUserData();
-      this.breadcrumbItems.push({
-        disabled: false,
-        href: "",
-        link: true,
-        text: "Characters"
-      })
     }
   }
 </script>
 <style lang="scss">
 .character-parent{
-  padding: 200px;
+  padding: 100px;
 }
 .content-wrapper{
 
@@ -110,5 +121,10 @@
   justify-content: center;
   align-items: center;
   height: 100vh;
+}
+.back-button{
+  position: absolute;
+  top: 40px;
+  left: 40px;
 }
 </style>
